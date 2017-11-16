a23c511bc4101df3d109037d9bf15c50a8361113
Jack <jacktbader5@gmail.com>
2017-11-16 12:24:53 -0700

    finished with a few pages

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Header'
+import './ErrorPage.css';
+
+const ErrorPage = () => {
+
+  return (
+    <div>
+      <Header />
+      <div>
+
+        <h1>Sorry that page doesnt exsist!</h1>
+
+      </div>
+    </div>
+  )
+
+}
+
+export default ErrorPage
