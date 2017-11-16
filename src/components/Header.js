2a63cde80a80209d1e50df3eb06b4cd23c290249
Jack Bader <jacktbader5@gmail.com>
2017-11-16 14:21:17 -0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -42,13 +42,6 @@ const Header = () => {
 
    </div>
 
-    <Navbar brand='FLEX' right>
-      <NavItem href='list-your-membership'>List a Membership</NavItem>
-      <NavItem href='components.html'>About</NavItem>
-      <NavItem href='components.html'>Sign Up</NavItem>
-      <NavItem href='get-started.html'>Login</NavItem>
-    </Navbar>
-
 
   )
 
