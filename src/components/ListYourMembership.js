7f30ae8f2a2af167da900379eb7f93fdea94937f
Jack Bader <jacktbader5@gmail.com>
2017-11-16 14:22:43 -0700

    Update ListYourMembership.js

--- src/components/ListYourMembership.js
+++ src/components/ListYourMembership.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
-<<<<<<< HEAD
 import {Button, Icon, Navbar, NavItem, Row, Input, Autocomplete} from 'react-materialize'
-=======
->>>>>>> a23c511bc4101df3d109037d9bf15c50a8361113
 import './ListYourMembership.css';
 
 const ListYourMembership = () => {
@@ -19,22 +16,15 @@ const ListYourMembership = () => {
         <div className="search-container">
           <div>
             <h1 className="list-your-membership-header" >List Your</h1>
-<<<<<<< HEAD
             <h1 className="list-your-membership-header" >Gym Membership</h1>
           </div>
         </div>
         <div className="list-your-membership-button-div">
           <a href="list"><Button href="list" className="search-button" waves='light'>List Your Membership</Button></a>
         </div>
-=======
-            <h1 className="list-your-membership-header">Gym Membership</h1>
-          </div>
-        </div>
->>>>>>> a23c511bc4101df3d109037d9bf15c50a8361113
       </div>
 
     </div>
-
 
   )
 
