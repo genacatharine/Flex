80bef2d42cb844192980385828774b0bae23b809
Gena Catherine <gena.catharine@gmail.com>
2017-11-16 12:32:51 -0700

    list css

--- src/components/List.js
+++ src/components/List.js
@@ -1,43 +1,26 @@
 import React from 'react'
 import {Button, Icon, Navbar, NavItem, Row, Input, Autocomplete} from 'react-materialize'
+import './List.css';
+import Header from './Header'
+var mdp = require('multipleDatePicker');
 
 const List = () => {
   return (
 <div>
-  <h1>LIST YOUR MEMBERSHIP RENTAL HERE</h1>
-  <form action="" method="POST">
-      <div class="form-line">
-          <label for="js-listingAddressInput">Which gym are you a member of?</label>
-            <input type="text" id="list-gym-dropdown" name="streetAddress" class="required text ui-autocomplete-input" autocomplete="off" data-specialty-details-required="" placeholder="Enter gym name" value="">
-      </div>
-            <div class="form-line form-line--largeTopMargin">
-                <div class="grid grid--withVerticalSpacing">
-                    <div class="grid-item grid-item--3">
-                        <label for="js-yearsInput">Start Day</label>
-                        <span class="styled-select-container styled-select-container--fluid styled-select-container--disabled">
-                            <select id="js-yearsInput" name="year" class="required" disabled="">
-                                <option value="">Select</option>
-                            </select>
-                            <span class="text placeholder">Select</span>
-                        </span>
-                    </div>
-
-                    <div class="grid-item grid-item--3">
-                        <label for="js-makesInput">End Day</label>
-                        <span class="styled-select-container styled-select-container--fluid styled-select-container--disabled">
-                            <select id="js-makesInput" name="make" class="required" disabled="">
-                                <option value="">Select</option>
-
-                            </select>
-                            <span class="text placeholder">Select</span>
-                        </span>
-                    </div>
-            <div class="buttonWrapper buttonWrapper--largeTopMargin">
-                <button type="submit" class="button button--purple js-submitButton">Next</button>
-            </div>
-            <a id="js-edmundsLink" class="edmundsLink" href="http://www.edmunds.com/?id=apis" target="_blank"><div class="js-edmundsLogo edmundsLogo edmundsLogo--invisible"></div></a>
-        </form>
-
+  <Header />
+  <h5>LIST YOUR MEMBERSHIP RENTAL HERE</h5>
+  <form>
+    <Row>
+	<Input class="selectgym" type='select' label="Select Your Gym" defaultValue='2'>
+		<option value='1'>Colorado Athletic Club</option>
+		<option value='2'>24 Hour Fitness</option>
+		<option value='3'>Boulder One Fitness</option>
+	</Input>
+</Row>
+<div>
+    <multiple-date-picker></multiple-date-picker>
+</div>
+</form>
 </div>
 )
 }
